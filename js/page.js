0824a5bde5f92a3d9b6bfaa514752ec49c57850a
yeyoung25 <sevim0105@gmail.com>
2023-11-16 22:49:05 +0900

    Update CARIBBEANBAY.html 231116 01

--- js/page.js
+++ js/page.js
@@ -1,3 +1,11 @@
-$(function(){
-    
+$(document).ready(function(){
+    justToggle($("div div:first-child nav div"));
+
 });
+
+function justToggle(button){
+$(button).click(function(){
+    $(this).toggleClass("active");
+});
+}
+  
