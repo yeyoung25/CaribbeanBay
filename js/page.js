92e7a850c79d6fd7a1a37a2a8a57dadca5f5c70b
yeyoung25 <sevim0105@gmail.com>
2023-11-17 01:15:09 +0900

    Update CARIBBEANBAY.html 231117 01

--- js/page.js
+++ js/page.js
@@ -1,11 +1,26 @@
 $(document).ready(function(){
-    justToggle($("div div:first-child nav div"));
-
+    navToggle($("div div:first-child nav div"));
+    newspage();
+    howTab();
 });
 
-function justToggle(button){
-$(button).click(function(){
-    $(this).toggleClass("active");
-});
+function navToggle(button){
+    $(button).click(function(){
+        $(this).toggleClass("active");
+    });
 }
-  
+
+function newspage(){
+    $(".lastP").click(function(e){
+        alert("This is the last page.");
+    });
+  };
+
+  function howTab(){
+    $('.howCBContainer div:nth-child(2) ul li').click(function(){
+      $('.howCBContainer div:nth-child(2) ul li').removeClass('active');
+      $(this).addClass('active');
+      $('.howCBContainer div:nth-child(2) div').removeClass('active');
+      $('.' + $(this).attr('data-tab')).addClass('active');
+    });
+  }
