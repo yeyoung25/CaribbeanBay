224491288fca03d161836ed0d6df7fb99429873d
yeyoung25 <sevim0105@gmail.com>
2023-11-26 01:56:32 +0900

    Update CARIBBEANBAY.html 231126 01

--- js/page.js
+++ js/page.js
@@ -46,6 +46,8 @@ function newspage(){
   function smoothtoAnchor(){
     const urlParams = new URL(location.href).searchParams;
     const scroll = urlParams.get('scroll');
-  
-    $('html,body').animate({ scrollTop: $("#" + scroll).offset()?.top - 150 }, 450, 'swing');
+    
+    if(scroll){
+      $('html,body').animate({ scrollTop: $("#" + scroll).offset()?.top - 150 }, 450, 'swing');
+    }
   }
